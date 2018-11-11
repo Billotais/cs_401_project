bf84a073d7dccd7fb893134ff96cc3f89c56dab5
Loïs Bilat <lois.bilat@gmail.com>
2018-11-11 15:33:39 +0100

    Added setting for HOME vs CLUSTER

--- script.py
+++ script.py
@@ -1,9 +1,17 @@
+CLUSTER = False
+
+if not CLUSTER:
+    import findspark
+    findspark.init()
 from pyspark.sql import *
-DATA_PATH = 'hdfs://datasets/amazon_multiling/tsv/'
+
+DATA_PATH = 'hdfs://datasets/amazon_multiling/tsv/' if CLUSTER else 'data/'
 
 spark = SparkSession.builder.getOrCreate()
 
-data_electronics = spark.read.csv(DATA_PATH + 'sample_us.tsv')
+#data_electronics = spark.read.csv(DATA_PATH + 'sample_us.tsv')
+data_electronics = spark.read.csv(DATA_PATH + 'amazon_reviews_multilingual_FR_v1_00.tsv')
+
 
 print(data_electronics.take(3))
 
