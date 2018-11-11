372db14003a3527edd67d08c06fbbd0ac3f0d919
Loïs Bilat <lois.bilat@epfl.ch>
2018-11-11 20:12:03 +0100

    Fixed on cluster

--- script.py
+++ script.py
@@ -1,17 +1,18 @@
-CLUSTER = False
+CLUSTER = True
 
 if not CLUSTER:
     import findspark
     findspark.init()
 from pyspark.sql import *
 
-DATA_PATH = 'hdfs://datasets/amazon_multiling/tsv/' if CLUSTER else 'data/'
+DATA_PATH = 'hdfs:///datasets/amazon_multiling/tsv/' if CLUSTER else 'data/'
 
 spark = SparkSession.builder.getOrCreate()
 
 #data_electronics = spark.read.csv(DATA_PATH + 'sample_us.tsv')
-data_electronics = spark.read.csv(DATA_PATH + 'amazon_reviews_multilingual_FR_v1_00.tsv')
+data_reviews = spark.read.csv(DATA_PATH + 'amazon_reviews_multilingual_FR_v1_00.tsv.gz')
 
-
-print(data_electronics.take(3))
+first_10 = data_reviews.take(10)
+for row in first_10:
+	print(row)
 
